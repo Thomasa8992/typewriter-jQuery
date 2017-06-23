5d58a12652f8a2563ba5bc328df2e8535d1fcef1
InnovateBham_6 <InnovateBham_6@admins-iMac.local>
2017-06-23 18:05:15 -0500

    shift works correctly

--- script.js
+++ script.js
@@ -1,23 +1,57 @@
+  
 
+
+var sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
+'Too ato too nOt enot one totA not anot tOO aNot', 
+'oat itain oat tain nate eate tea anne inant nean', 
+'itant eate anot eat nato inate eat anot tain eat' , 
+'nee ene ate ite tent tiet ent ine ene ete ene ate'];
+
+
+$('#keyboard-upper-container').hide();
 $( document ).ready(function() {
-    $('#keyboard-upper-container').hide();
     //switches between lowercase and uppercase
     $(document).on('keydown',function(e){
-        var keycode = 16;
-        if (e.which === keycode) {
+        if (e.which === 16) {
             $('#keyboard-lower-container').hide();
             $('#keyboard-upper-container').show();
+            console.log(e.which);
         }
-    }).on('keyup',function(){
+    $(document).on('keyup',function(e){
+        if (e.which === 16) {
         $('#keyboard-upper-container').hide();
         $('#keyboard-lower-container').show();
-    }); 
-    $(document).keypress(function(e){
-        var id = $('#id');
-        if(e.which === id) {
-            // $('#id').addClass('#yellow-block');
-            console.log();
         }
-    });
+    }); 
 });
+$('#sentence').append(sentences[0]);
+
+
+
+    $(document).on('keydown', function(e) {
+        if (e.which == 16){
+            e.which +32;
+            handleHighlight(e.which);
+        } else {
+        handleHighlight(e.which);
+        }
+        });
+    $(document).on('keyup', function(e) {
+        handleHighlight(e.which + 32, true);
+        if (e.which == 16) {
+            handleHighlight(e.which);
+        } else {
+        handleHighlight(e.which);
+        }
+    
+    });    
+});
+
+function handleHighlight(code, defaultColor){
+        if (defaultColor){
+            $('#' + code).css('background-color', '#f5f5f5');
+        } else {
+            $('#' + code).css('background-color', 'yellow');
+        }
+}
 

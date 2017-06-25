935b4958ba0406285c3ce42e97487762801ed0db
InnovateBham_6 <InnovateBham_6@admins-iMac.local>
2017-06-25 14:29:00 -0500

    keyboard works

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ var sentences = ['ten ate neite ate nee enet ite ate inet ent eate',
 'itant eate anot eat nato inate eat anot tain eat' , 
 'nee ene ate ite tent tiet ent ine ene ete ene ate'];
 
+var str1 = sentences[0];
+var str2 = sentences[1];
+var str3 = sentences[2];
+var str4 = sentences[3];
+var str5 = sentences[4];
 
 $('#keyboard-upper-container').hide();
 $( document ).ready(function() {
@@ -16,64 +21,60 @@ $( document ).ready(function() {
             $('#keyboard-lower-container').hide();
             $('#keyboard-upper-container').show();
         }
+    }); 
     $(document).on('keyup',function(e){
         if (e.which === 16) {
         $('#keyboard-upper-container').hide();
         $('#keyboard-lower-container').show();
         }
-    }); 
-});
-
-
-
-
-
-
-
-
-
-
+    });
     $(document).on('keydown', function(e) {
         // if (e.which >= 65 || e.which <= 90 || e.which === 32 && e.which == e.shiftKey) {
-        //         console.log(e.which);
+        //         console.log(e.charCode + 65);
         // } 
-        // if(e.which === 48 || e.which >= 48 || e.which <= 57) {
-        //     addHighlight(e.which);
-        // }
-        // if(e.which >= 65 || e.which <= 90){
-        //      addHighlight(e.which + 32);
-        // }
+        if(e.which === 48 || e.which >= 48 || e.which <= 57) {
+            addHighlight(e.which);
+        }
+    //     if (e.shiftKey && e.which === 49){
+    //         addHighlight(e.preventDefualt);
+    // }
+        if(e.which >= 65 || e.which <= 90){
+             addHighlight(e.which + 32);
+        }
         // if (e.whcih >= 51 || e.whcih >= 53 && e.shiftKey){
         //     addHighlight(e.which - 18);
-        // }
-        // if (e.which === 49 && e.shiftKey){
-        //     addHighlight(e.which -16);
+        // if(e.shiftKey == false){
+        //     addHighlight(e.charCode);
+        if (e.which === 49 && e.shiftKey){
+            addHighlight(e.which -16);
+        }
         // }
      });    
-
     $(document).on('keyup', function(e) {
-        // if (e.which >= 65 || e.which <= 90 || e.which >= 123 || e.which <= 126 && e.which == 16) {
-        //         removeHighlight(e.which);
-        // }
-        // if(e.which === 48 || e.which >= 48 || e.which <= 57) {
-        //     removeHighlight(e.which);
-        // }
+        if (e.which >= 65 || e.which <= 90 || e.which >= 123 || e.which <= 126 && e.shiftKey) {
+                removeHighlight(e.which);
+        }
+        if(e.which === 48 || e.which >= 48 || e.which <= 57) {
+            removeHighlight(e.which);
+        }
+        if(e.which >= 65 && e.which <= 90){
+             removeHighlight(e.which + 32);
+        }
         //  if(e.which >= 65 || e.which <= 90){
         //      removeHighlight(e.which + 32);
         // } 
-        // if (e.which === 49 && e.shiftKey){
-        //     removeHighlight(e.which -16);
-        // }
-    });  
+        if (e.which === 49 && e.shiftKey){
+            removeHighlight(e.which -16);
+        }
+    }); 
+     $(document).on('keypress', function(e) {
+        if(e.which){
+            appendLetter(e.which);
+        }
+     }); 
+    $('#sentence').append(str1);  
 
-
-
-
-
-
-    $('#sentence').append(sentences[0]);  
 });
-
 function addHighlight(code){
         // if (defaultColor){
         //     $('#' + code).css('background-color', '#f5f5f5');
@@ -81,7 +82,6 @@ function addHighlight(code){
             $('#' + code).css('background-color', 'yellow');
         // }
 }
-
 function removeHighlight(code, defaultColor){
         // if (defaultColor){
         //     $('#' + code).css('background-color', '#f5f5f5');
@@ -90,14 +90,8 @@ function removeHighlight(code, defaultColor){
         // }
 }
 
-// function between(x, min, max) {
-//   return x >= min && x <= max;
-// }
-// // ...
-// function between(x, min, max) {
-//   return x >= min && x <= max;
-// }
-// // ...
-// if (between(x, 0.001, 0.009)) {
-//   // something
-// }
+function appendLetter (code){
+    if(code == 116){
+        $('#target-letter').append(String.fromCharCode(code));
+    } 
+}
